171957c9860f012622799fdc8caffd8b73fa7fa5
kjlQ <rett8716@gmail.com>
2021-03-30 23:48:35 +0300

    adaptation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React,{ useState , useRef , useEffect } from 'react';
+import TodoList from './TodoList'
+import uuid from 'uuid/dist/v4'
+import './App.css'
+
+const LOCAL_KEY_APLIC = "todoApp"
+
+export default function App(e) {
+  const [todos, setTodos] = useState ([])
+  const todoNameRef = useRef()
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_KEY_APLIC))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_KEY_APLIC,JSON.stringify(todos))
+  }, [todos])
+  function toggleTodo(id) {
+    const newTodos = [...todos];
+    const todo = newTodos.find(todo => todo.id === id);
+    todo.complete = !todo.complete;
+    setTodos(newTodos)
+  }
+  function handleAddName () {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    todoNameRef.current.value = null
+    setTodos(prevTodos => {
+      return [...prevTodos , {id:uuid() , name: name , complete:false}]
+    })
+  }
+  function all() {
+    setTodos(todos.map(todo=> {
+      return {id:todo.id , name : todo.name ,complete:true}
+    }))
+  }
+  function unall() {
+    setTodos(todos.map(todo => {
+      return {id:todo.id,name:todo.name,complete:false}
+    }))
+  }
+  function clearTodo() {
+    const newTodos = todos.filter(todo=>!todo.complete)
+    setTodos(newTodos)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={"wid"}>
+        <div className={"functionality"}>
+          <p>{todos.filter(todos=>!todos.complete).length} todo left</p>
+          <form>
+            <input type="text" ref={todoNameRef}/>
+            <button onClick={handleAddName}>Add todo</button>
+          </form>
+        </div>
+        <div className="buttons">
+          <button onClick={clearTodo}>Clear</button>
+          <button onClick={all}>Mark all</button>
+          <button onClick={unall}>Unmark all</button>
+        </div>
+        <div className="todoList">
+          <div className="todos">
+            All todo list :
+            <TodoList todos = {todos} toggleTodo={toggleTodo} />
+          </div>
+        </div>
+      </div>
   );
 }
 
-export default App;
